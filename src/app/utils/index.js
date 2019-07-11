8a66ffd84e6ec2e8fcafadb761f5e1a89ede25b7
Javi <arancibiajav@gmail.com>
2019-07-11 17:13:13 +0200

    Disable cache for specific scripts

--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -1,39 +1,45 @@
 module.exports = {
-  cacheCDNScripts
+    cacheCDNScripts
 }
 
-async function cacheCDNScripts (html) {
-  var sander = require('sander')
-  var path = require('path')
-  var axios = require('axios')
-  const cheerio = require('cheerio')
-  const $ = cheerio.load(html)
-  var urlItems = []
-  $('script').each(function () {
-    let url = $(this).attr('src')
-    urlItems.push({
-      fileName: url.substring(url.lastIndexOf('/') + 1),
-      url: url,
-      el: $(this)
+async function cacheCDNScripts(html) {
+    var sander = require('sander')
+    var path = require('path')
+    var axios = require('axios')
+    const cheerio = require('cheerio')
+    const $ = cheerio.load(html)
+    var urlItems = []
+    $('script').each(function() {
+        let url = $(this).attr('src')
+        urlItems.push({
+            fileName: url.substring(url.lastIndexOf('/') + 1),
+            url: url,
+            el: $(this)
+        })
     })
-  })
-  urlItems = urlItems.filter(urlItem => urlItem.url.indexOf('http') !== -1)
-  let cachedFileNames = await sander.readdir(`dist/cache`)
-  await Promise.all( 
-    urlItems.map(urlItem =>
-      (async () => {
-        let exists = cachedFileNames.includes(urlItem.fileName);
-        if (!exists) {
-          console.log(`Caching ${urlItem.fileName}`.grey);
-          try {
-            let raw = await axios.get(urlItem.url)
-            sander.writeFile(`dist/cache/${urlItem.fileName}`, raw.data);
-            urlItem.el.attr('src', `/cache/${urlItem.fileName}`);
-          } catch (err) {}
+    urlItems = urlItems.filter(urlItem => {
+        if ((urlItem.el.attr('cache') || '').toString() === "0") {
+            //console.log(`NO CACHE FOR ${urlItem.fileName}`.red.bgWhite)
+            return false;
         }
-      })()
-    )
-  );
-  html = $.html();
-  return html;
+        return urlItem.url.indexOf('http') !== -1
+    })
+    let cachedFileNames = await sander.readdir(`dist/cache`)
+    await Promise.all(
+        urlItems.map(urlItem =>
+            (async() => {
+                let exists = cachedFileNames.includes(urlItem.fileName);
+                if (!exists) {
+                    console.log(`Caching ${urlItem.fileName}`.grey);
+                    try {
+                        let raw = await axios.get(urlItem.url)
+                        sander.writeFile(`dist/cache/${urlItem.fileName}`, raw.data);
+                        urlItem.el.attr('src', `/cache/${urlItem.fileName}`);
+                    } catch (err) {}
+                }
+            })()
+        )
+    );
+    html = $.html();
+    return html;
 }
