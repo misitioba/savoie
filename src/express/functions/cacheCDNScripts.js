a814ac58e90d50b8fd60eaba8f3a582b813f3aaf
Javi <arancibiajav@gmail.com>
2019-07-12 11:51:09 +0200

    Implemented modules

---
+++ src/express/functions/cacheCDNScripts.js
@@ -0,0 +1,46 @@
+module.exports = app => {
+    return async function cacheCDNScripts(html) {
+        var sander = require('sander')
+        var path = require('path')
+        var axios = require('axios')
+        const cheerio = require('cheerio')
+        const $ = cheerio.load(html)
+        var urlItems = []
+        $('script').each(function() {
+            let url = $(this).attr('src') || "";
+            urlItems.push({
+                fileName: url.substring(url.lastIndexOf('/') + 1),
+                url: url,
+                el: $(this)
+            })
+        })
+        urlItems = urlItems.filter(urlItem => {
+            if (!urlItem.url) {
+                return false;
+            }
+            if ((urlItem.el.attr('cache') || '').toString() === '0') {
+                // console.log(`NO CACHE FOR ${urlItem.fileName}`.red.bgWhite)
+                return false
+            }
+            return urlItem.url.indexOf('http') !== -1
+        })
+        let cachedFileNames = await sander.readdir(`dist/cache`)
+        await Promise.all(
+            urlItems.map(urlItem =>
+                (async() => {
+                    let exists = cachedFileNames.includes(urlItem.fileName)
+                    if (!exists) {
+                        console.log(`Caching ${urlItem.fileName}`.grey)
+                        try {
+                            let raw = await axios.get(urlItem.url)
+                            sander.writeFile(`dist/cache/${urlItem.fileName}`, raw.data)
+                            urlItem.el.attr('src', `/cache/${urlItem.fileName}`)
+                        } catch (err) {}
+                    }
+                })()
+            )
+        )
+        html = $.html()
+        return html
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,45 +1,86 @@
-require('dotenv').config({ silent: true });
+require('dotenv').config({ silent: true })
 
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
 
-require('colors');
-require('./express/functions')(server);
-require('./app/generator');
+require('colors')
+require('./express/functions')(server)
 
-//parse json
-const bodyParser = require('body-parser');
-server.use(bodyParser.json({
-    limit: '50mb'
-}));
+// parse json
+const bodyParser = require('body-parser')
+server.use(
+    bodyParser.json({
+        limit: '50mb'
+    })
+)
 
-//mysql session
-server.use(server.getMysqlSessionMiddleware({
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD,
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DATABASE
-}));
-
-server.use((req, res, next) => {
-    server.functions.forEach(functionName => req[functionName] = server[functionName]);
-    next();
-});
-
+// mysql session
+server.use(
+    server.getMysqlSessionMiddleware({
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PWD,
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        database: process.env.MYSQL_DATABASE
+    })
+)
 
 server.use((req, res, next) => {
-            var debug = require('debug')(`app:request ${`${Date.now()}`.white}`);
+    server.functions.forEach(
+        functionName => (req[functionName] = server[functionName])
+    )
+    next()
+})
+
+server.use((req, res, next) => {
+            var debug = require('debug')(`app:request ${`${Date.now()}`.white}`)
   debug(`${req.url}`)
-  next();
-});
+  next()
+})
 
-server.use('/', express.static('dist'));
-server.use('/static', express.static('src/static'));
-server.use('/admin_pages', express.static('src/app/pages'));
-server.use('/styles', express.static('src/app/styles'));
-server.use('/api',require('./express/api'));
-var PORT = process.env.PORT || 3000;
+server.use('/', express.static('dist'))
+server.use('/static', express.static('src/static'))
+server.use('/admin_pages', express.static('src/app/pages'))
+server.use('/styles', express.static('src/app/styles'))
+server.use('/api', require('./express/api'))
 
-var debug = require('debug')(`app:server ${`${Date.now()}`.white}`);
-server.listen(PORT,()=>debug(`Listening at ${PORT}`))
+generateProject().then(() => {
+  loadModules().then(listen)
+})
+
+function generateProject () {
+  return require('./app/generator')(server) // promise
+}
+
+async function loadModules () {
+  let conn = await server.getMysqlConnection()
+  var debug = require('debug')(`app:modules ${`${Date.now()}`.white}`)
+  let [modules, fields] = await conn.execute(`SELECT * FROM modules`, [])
+  let promises = modules.map(module => {
+    return (async function () {
+      let name = module.title
+        .split(' ')
+        .join('-')
+        .toLowerCase();
+      server.builder.distFolder = `dist/${name}`;
+      server.builder.cwd = module.path;
+      try {
+        await require(module.path)(server);
+        debug(`${module.title} loaded as ${name}`);
+      } catch (err) {
+        debug(`${module.title} load error`, {
+          err: err.stack
+        });
+      }
+      server.builder.distFolder = '';
+      server.builder.cwd = '';
+    })();
+  });
+  await Promise.all(promises);
+}
+
+function listen () {
+  var PORT = process.env.PORT || 3000
+  var debug = require('debug')(`app:server ${`${Date.now()}`.white}`)
+  server.listen(PORT, () => debug(`Listening at ${PORT}`))
+}
