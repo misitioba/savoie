8c4474e2d3cd95fd497262b65636cdf5ae55a795
Javi <arancibiajav@gmail.com>
2019-07-19 17:55:28 +0200

    client side intragration

--- src/express/functions/configureFunql.js
+++ src/express/functions/configureFunql.js
@@ -11,43 +11,55 @@ function clousureEval(_evalCode, _scope) {
 module.exports = app => {
         var debug = require('debug')(`app:express:funql ${`${Date.now()}`.white}`)
   return function configureFunql() {
+    app.get('/funql', async function configureFunqlRoute(req, res) {
+      let data = JSON.parse(require('atob')(req.query.body))
+      if (data.transformEncoded) {
+        data.transform = require('atob')(data.transform)
+      }
+      await executeFunql(data, res)
+    })
+
     app.post('/funql', async function configureFunqlRoute(req, res) {
       let data = req.body
-      let name = data.name
-      if (!app.api[name]) {
-        res.json({
-          err: 'INVALID_NAME',
-        })
-      } else {
-        try {
-          let result = await app.api[name].apply(app.api, data.args || [])
-          if (data.transform && typeof data.transform === 'string') {
-            var transformHandler = (result, options = {}) => {
-              var moment = require('moment')
-              var __handler = {}
-              clousureEval(`__handler.fn = ${data.transform};`, {
-                __handler,
-                moment: require('moment'),
-              })
-              return __handler.fn(result)
-            }
-
-            let transformed = transformHandler(result)
-
-            if (transformed instanceof Promise) {
-              result = await transformed
-            } else {
-              result = transformed
-            }
-          }
-          res.json(result)
-        } catch (err) {
-          debug(err.stack.red)
-          res.json({
-            err: '500',
-          })
-        }
-      }
+      await executeFunql(data, res)
     })
+  }
+
+  async function executeFunql(data, res) {
+    let name = data.name
+    if (!app.api[name]) {
+      res.json({
+        err: 'INVALID_NAME',
+      })
+    } else {
+      try {
+        let result = await app.api[name].apply(app.api, data.args || [])
+        if (data.transform && typeof data.transform === 'string') {
+          var transformHandler = (result, options = {}) => {
+            var moment = require('moment')
+            var __handler = {}
+            clousureEval(`__handler.fn = ${data.transform};`, {
+              __handler,
+              moment: require('moment'),
+            })
+            return __handler.fn(result)
+          }
+
+          let transformed = transformHandler(result)
+
+          if (transformed instanceof Promise) {
+            result = await transformed
+          } else {
+            result = transformed
+          }
+        }
+        res.json(result)
+      } catch (err) {
+        debug(err.stack.red)
+        res.json({
+          err: '500',
+        })
+      }
+    }
   }
 }
--- src/express/functions/index.js
+++ src/express/functions/index.js
@@ -1,42 +1,52 @@
 var debug = require('debug')(`app:express:functions ${`${Date.now()}`.white}`)
 
-var sander = require('sander')
-let files = sander.readdirSync(__dirname)
-files = files
-  .filter(f => f !== 'index.js')
-  .filter(f => {
-    return f.indexOf('.js') !== -1
-  })
 module.exports = app => {
-  var self = {}
-  files.forEach(f => {
-    self[f.split('.')[0]] = require(__dirname + '/' + f)
+  app.loadFunctions = createLoadFunctions(app)
+  app.loadFunctions({
+    path: __dirname
   })
-  Object.keys(self)
-    .map((k, index) => {
-      var mod = self[k]
-      return {
-        name: k,
-        handler: mod.handler ? mod.handler : mod
-      }
+}
+
+function createLoadFunctions (app) {
+  return function loadFunctions (options) {
+    var folderPath = options.path || __dirname
+    var sander = require('sander')
+    let files = sander.readdirSync(folderPath)
+    files = files
+      .filter(f => f !== 'index.js')
+      .filter(f => {
+        return f.indexOf('.js') !== -1
+      })
+    var self = {}
+    files.forEach(f => {
+      self[f.split('.')[0]] = require(folderPath + '/' + f)
     })
-    .forEach(fn => {
-      let impl = fn.handler(app)
-      if (impl instanceof Promise) {
-        impl.then(handler => onReady(app, fn, handler)).catch(onError)
-      } else {
-        onReady(app, fn, impl)
-      }
-    })
+    Object.keys(self)
+      .map((k, index) => {
+        var mod = self[k]
+        return {
+          name: k,
+          handler: mod.handler ? mod.handler : mod
+        }
+      })
+      .forEach(fn => {
+        let impl = fn.handler(app)
+        if (impl instanceof Promise) {
+          impl.then(handler => onReady(app, fn, handler)).catch(onError)
+        } else {
+          onReady(app, fn, impl)
+        }
+      })
+  }
 }
 
 function onReady (app, fn, impl) {
   if (typeof app[fn.name] !== 'undefined') {
     debug('Function file', fn.name, 'exists. Skipping...')
   } else {
-    app.functions = app.functions || [];
-    app.functions.push(fn.name);
-    //debug('Function file', fn.name, 'loaded')
+    app.functions = app.functions || []
+    app.functions.push(fn.name)
+    // debug('Function file', fn.name, 'loaded')
     app[fn.name] = function () {
       let r = impl.apply(this, arguments)
       if (r instanceof Promise) {
@@ -46,16 +56,13 @@ function onReady (app, fn, impl) {
             debug(
               'call',
               fn.name,
-              r instanceof Array ? ('Responded with '+r.length + ' items') : `Responded with object ${printKeys(r)}`
+              r instanceof Array
+                ? 'Responded with ' + r.length + ' items'
+                : `Responded with object ${printKeys(r)}`
             )
             resolve(r)
           } catch (err) {
-            debug(
-              'call',
-              fn.name,
-              `Responded with error`,
-              `${err.stack}`.red
-            )
+            debug('call', fn.name, `Responded with error`, `${err.stack}`.red)
             reject(err)
           }
         })
@@ -63,13 +70,14 @@ function onReady (app, fn, impl) {
         debug(
           'call',
           fn.name,
-          r instanceof Array ? ('Responded with '+r.length + ' items') : `Responded with object ${printKeys(r)}`
+          r instanceof Array
+            ? 'Responded with ' + r.length + ' items'
+            : `Responded with object ${printKeys(r)}`
         )
         return r
       }
     }
   }
-  
 }
 
 function onError (err) {
@@ -77,16 +85,16 @@ function onError (err) {
   process.exit(1)
 }
 
-function printKeys(object = {}){
-  if(!object){
-    return object;
+function printKeys (object = {}) {
+  if (!object) {
+    return object
   }
-  let keys = Object.keys(object);
-  if(keys.length>10){
-    let count = keys.length;
-    keys = keys.filter((k,index)=>index<10);
+  let keys = Object.keys(object)
+  if (keys.length > 10) {
+    let count = keys.length
+    keys = keys.filter((k, index) => index < 10)
     return `{${keys.join(', ')}... (${count} more)}`
-  }else{
+  } else {
     return `{${keys.join(', ')}}`
   }
 }
