3a6234ddbb7d03281ac5b51a32b8625042acfbd3
Javi <arancibiajav@gmail.com>
2019-08-17 13:33:12 +0200

    send an email about password change

--- src/express/funql_api/changeUserPassword.js
+++ src/express/funql_api/changeUserPassword.js
@@ -3,6 +3,22 @@ module.exports = app => {
   return async function changeUserPassword ({ newPassword }) {
     if (!!newPassword && this.req.user) {
       await app.editUserPassword(this.req.user.email, newPassword, newPassword)
+
+      let mailgun = await app.getMailgun()
+      mailgun.messages().send(
+        {
+          from:
+            'misitioba (Tech Cooperative at Savoie) <no-reply@misitioba.com>',
+          to: this.req.user.email,
+          subject: 'Vous avez changé votre mot de passe',
+          text: `Votre nouveau mot de passe est ${newPassword}`
+        },
+        function (error, body) {
+          if (error) {
+            debug(`while sending email`, error)
+          }
+        }
+      )
     }
   }
 }
