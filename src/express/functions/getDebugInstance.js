b26c6f65b9e8364572758075510171a745a933f1
Javi <arancibiajav@gmail.com>
2019-10-24 15:25:08 +0200

    DEBUG_LEVEL added

--- src/express/functions/getDebugInstance.js
+++ src/express/functions/getDebugInstance.js
@@ -1,6 +1,19 @@
 module.exports = app =>
-    function getDebugInstance(name) {
-        return require('debug')(
-                `${`app:${name}`.padEnd(15, ' ')} ${`${Date.now()}`.white}`
-    )
+    function getDebugInstance(name, level = 4) {
+        let levelLabel = {
+            1: 'ERROR'.red,
+            2: 'WARN'.yellow,
+            3: 'INFO'.blue,
+            4: 'DEBUG'.grey
+        }
+        let _level = parseInt(process.env.DEBUG_LEVEL) || 4
+        if (level > _level) {
+            return () => {}
+        } else {
+            return require('debug')(
+                    `${`app:${name}`.padEnd(15, ' ')} ${levelLabel[level].padEnd(5, ' ')} ${
+          `${Date.now()}`.white
+        }`
+      )
+    }
   }
